6e22d00241a233db335ce9c3ffde6fbf8df5bd50
Brian Brewer <up672363@myport.ac.uk>
2015-10-25 23:01:35 +0000

    Moved things around to stop script hanging

--- script.js
+++ script.js
@@ -1,58 +1,53 @@
-window.addEventListener("load", function () {
-  _backgroundMusic = document.getElementById("backgroundMusic"); // Audio element
-  _backgroundMusicControl = document.querySelector(".backgroundMusicControl"); // Audio toggle button
-  _pathItemGoals = document.querySelectorAll(".pathItemGoal"); // All working goals
-  _pathGoalPopup = document.querySelector(".pathGoalPopup"); // Popup showing the goal information
+var _backgroundMusic = document.getElementById("backgroundMusic"); // Audio element
+var _backgroundMusicControl = document.querySelector(".backgroundMusicControl"); // Audio toggle button
+var _pathItemGoals = document.querySelectorAll(".pathItemGoal"); // All working goals
+var _pathGoalPopup = document.querySelector(".pathGoalPopup"); // Popup showing the goal information
 
-  _pathItemGoalCallback = function (e) {
-    // Show popup and move to goal
-    _pathGoalPopup.classList.remove("hidden");
-    _pathGoalPopup.style.left = this.style.left;
-    _pathGoalPopup.style.top = this.style.top;
+_pathItemGoalCallback = function (e) {
+  // Show popup and move to goal
+  _pathGoalPopup.classList.remove("hidden");
+  _pathGoalPopup.style.left = this.style.left;
+  _pathGoalPopup.style.top = this.style.top;
 
-    // Grab stats for the popup from the goal clicked
-    var data = e.target.dataset;
+  // Grab stats for the popup from the goal clicked
+  var data = e.target.dataset;
+  _pathGoalPopup.querySelector(".current").innerHTML = data.current;
+  _pathGoalPopup.querySelector(".goal").innerHTML = data.goal;
+  _pathGoalPopup.querySelector(".title").innerHTML = data.title;
+  _pathGoalPopup.querySelector(".description").innerHTML = data.description;
+};
 
+// Remember setting for music on/off (Yeah, it's pedantic)
+if (window.localStorage.musicToggle === "false") {
+  _backgroundMusic.pause();
+  _backgroundMusicControl.querySelector("img").src = "http://media.overkillsoftware.com/2015/01/bom_music_off.png";
+}
 
-    _pathGoalPopup.querySelector(".current").innerHTML = data.current;
-    _pathGoalPopup.querySelector(".goal").innerHTML = data.goal;
-    _pathGoalPopup.querySelector(".title").innerHTML = data.title;
-    _pathGoalPopup.querySelector(".description").innerHTML = data.description;
-
-  };
-
-  // Remember setting for music on/off (Yeah, it's pedantic)
-  if (window.localStorage.musicToggle === "false") {
+// Toggle background music and image
+_backgroundMusicControl.addEventListener("click", function () {
+  if (_backgroundMusic.paused) {
+    window.localStorage.musicToggle = true;
+    _backgroundMusic.play();
+    _backgroundMusicControl.querySelector("img").src = "http://media.overkillsoftware.com/2015/01/bom_music_on.png";
+  } else {
+    window.localStorage.musicToggle = false;
     _backgroundMusic.pause();
     _backgroundMusicControl.querySelector("img").src = "http://media.overkillsoftware.com/2015/01/bom_music_off.png";
   }
+});
 
-  // Toggle background music and image
-  _backgroundMusicControl.addEventListener("click", function () {
-    if (_backgroundMusic.paused) {
-      window.localStorage.musicToggle = true;
-      _backgroundMusic.play();
-      _backgroundMusicControl.querySelector("img").src = "http://media.overkillsoftware.com/2015/01/bom_music_on.png";
-    } else {
-      window.localStorage.musicToggle = false;
-      _backgroundMusic.pause();
-      _backgroundMusicControl.querySelector("img").src = "http://media.overkillsoftware.com/2015/01/bom_music_off.png";
-    }
-  });
+// Attach handler to all working goals
+for (i = 0, j = _pathItemGoals.length; i < j; i += 1) {
+  _pathItemGoals[i].addEventListener("click", _pathItemGoalCallback);
+}
 
-  // Attach handler to all working goals
-  for (i = 0, j = _pathItemGoals.length; i < j; i += 1) {
-    _pathItemGoals[i].addEventListener("click", _pathItemGoalCallback);
-  }
+// Attach close button logic
+_pathGoalPopup.querySelector(".close").addEventListener("click", function () {
+  _pathGoalPopup.classList.add("hidden");
+});
 
-  // Attach close button logic
-  _pathGoalPopup.querySelector(".close").addEventListener("click", function () {
+$(document).bind('click', function(e) {
+  if(!$(e.target).is(_pathGoalPopup) && !$(e.target).is(".pathItemGoal")) {
     _pathGoalPopup.classList.add("hidden");
-  });
-  
-  $(document).bind('click', function(e) {
-    if(!$(e.target).is(_pathGoalPopup) && !$(e.target).is(".pathItemGoal")) {
-      _pathGoalPopup.classList.add("hidden");
-    }
-  });
+  }
 });
